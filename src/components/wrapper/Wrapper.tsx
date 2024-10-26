123cb32f73580da7afe40b0aee7083565d0abec2
Jenya <113167825+Genya10@users.noreply.github.com>
2024-10-26 21:24:08 +0300

    install mobx-utils and use it.

--- src/components/wrapper/Wrapper.tsx
+++ src/components/wrapper/Wrapper.tsx
@@ -10,22 +10,26 @@ const counter2 = new CounterStore()
 const counter3 = new CounterStore()
 
 export const Wrapper = observer(()=> {
-    const {getPostsAction, posts, isLoading} = PostsStore
+    const {getPostsAction, posts} = PostsStore
 
     useEffect(()=> {
         getPostsAction()
     },[])
 
+    if(posts?.state === 'pending'){
+        return <div>loading</div>
+    }
+
+    if(posts?.state === 'rejected'){
+        return <div>error</div>
+    }
+    
     console.log(posts, 'posts')
 
     return (
-     <>
-       <div className={cl.wrapper}>
-        <div className={cl.counter}> <Counter {...counter1} total={counter1.total}/></div>
-        <div className={cl.counter}> <Counter {...counter2} total={counter2.total}/></div> 
-        <div className={cl.counter}> <Counter {...counter3} total={counter3.total}/></div>
-        </div>
-     </>
+     <h2>
+     {posts?.value[0].body}
+     </h2>
     )
 })
 
--- src/stores/posts-store.tsx
+++ src/stores/posts-store.tsx
@@ -1,26 +1,16 @@
 import { makeAutoObservable, runInAction } from "mobx"
 import { getPosts, PropsPosts} from '../api/getPosts'
+import { fromPromise, IPromiseBasedObservable } from "mobx-utils"
 
 class PostsStore {
-    posts: PropsPosts[] = []
-    isLoading = false
+    posts?: IPromiseBasedObservable<PropsPosts[]>
 
     constructor(){
         makeAutoObservable(this)
     }
 
     getPostsAction = async() => {
-        try {
-            this.isLoading = true
-            const res = await getPosts()
-
-            runInAction(()=> {
-                this.posts = res
-                this.isLoading = false
-            })
-        } catch {
-            this.isLoading = false
-        }
+     this.posts = fromPromise(getPosts())
     }
 }
 
